9f77c123122087dafd0dc0caf7a726e7c4c8365c
tedasi <tenorio.felipe.dasilva@gmail.com>
2025-10-03 10:19:55 +0200

    propped sin comentarios

--- src/baseNoStates/Propped.java
+++ src/baseNoStates/Propped.java
@@ -1,35 +1,23 @@
 package baseNoStates;
 
-/**
- * Represents the 'Propped' state of a door.
- * This state typically means the door is being held open by an external object.
- * Closing the door from this state will also lock it.
- */
+
 public class Propped extends DoorState {
-  /**
-   * Constructs a Propped state object.
-   * @param door The door this state belongs to.
-   */
+
   public Propped(Door door) {
     super(door);
   }
 
-  /**
-   * Returns the name of the state.
-   * @return The string "propped".
-   */
+
   @Override
   public String getStateName() {
     return "propped";
   }
 
-  // Action to open the door. Does nothing, as the door is already open.
   @Override
   public void open() {
     System.out.println("The door " + door.getId() + " is already open and propped.");
   }
 
-  // Action to close the door. This action not only closes the door but also immediately transitions it to the Locked state.
   @Override
   public void close() {
     door.setClosed(true);
@@ -38,25 +26,21 @@ public class Propped extends DoorState {
     System.out.println("The door " + door.getId() + " is now locked (after being propped).");
   }
 
-  // Action to lock the door. Fails because a propped door must be closed first.
   @Override
   public void lock() {
     System.out.println("Can't lock the door " + door.getId() + " directly; must close it first.");
   }
 
-  // Action to unlock the door. Fails because the door is not locked.
   @Override
   public void unlock() {
     System.out.println("Can't unlock the door " + door.getId() + "; it's already propped open.");
   }
 
-  // Action to unlock the door temporarily. Fails because the door is not locked.
   @Override
   public void unlockShortly() {
     System.out.println("Can't unlock the door " + door.getId() + " shortly; it's already propped.");
   }
 
-  // Action to prop the door. Does nothing, as it's already in the Propped state.
   @Override
   public void propped() {
     System.out.println("The door " + door.getId() + " is already propped.");
--- src/baseNoStates/UnlockedShortly.java
+++ src/baseNoStates/UnlockedShortly.java
@@ -1,30 +1,18 @@
 package baseNoStates;
 
-/**
- * Represents a temporary 'Unlocked' state.
- * Upon entering this state, a timer starts. When the timer expires, if the door
- * is closed, it automatically transitions to the 'Locked' state.
- */
 public class UnlockedShortly extends DoorState {
-  /**
-   * Constructs an UnlockedShortly state object and starts the auto-lock timer.
-   * @param door The door this state belongs to.
-   */
+
   public UnlockedShortly(Door door) {
     super(door);
     startLockTimer();
   }
 
-  /**
-   * Returns the name of the state.
-   * @return The string "unlocked_shortly".
-   */
+
   @Override
   public String getStateName() {
     return "unlocked_shortly";
   }
 
-  // Action to open the door. Succeeds if the door is physically closed.
   @Override
   public void open() {
     if (door.isClosed()) {
@@ -35,7 +23,6 @@ public class UnlockedShortly extends DoorState {
     }
   }
 
-  // Action to close the door. Succeeds if the door is physically open.
   @Override
   public void close() {
     if (door.isClosed()) {
@@ -46,7 +33,6 @@ public class UnlockedShortly extends DoorState {
     }
   }
 
-  // Action to lock the door. Succeeds if the door is closed, transitioning it to the Locked state.
   @Override
   public void lock() {
     if (door.isClosed()) {
@@ -57,29 +43,21 @@ public class UnlockedShortly extends DoorState {
     }
   }
 
-  // Action to unlock the door. Does nothing, as it is already in an unlocked state.
   @Override
   public void unlock() {
     System.out.println("Can't unlock the door " + door.getId() + ", it's already unlocked (shortly).");
   }
 
-  // Action to unlock the door temporarily. Does nothing, as it is already in this state.
   @Override
   public void unlockShortly() {
     System.out.println("The door " + door.getId() + " is already temporarily unlocked.");
   }
 
-  // Action to prop the door. This action has no effect in this state.
   @Override
   public void propped() {
-    // This action is not applicable in this state.
   }
 
-  /**
-   * Starts a background thread that waits for a specific duration (10 seconds).
-   * After the delay, if the door is still closed, it changes its state to Locked.
-   * If the thread is interrupted, it transitions the door to the Propped state.
-   */
+
   private void startLockTimer() {
     new Thread(() -> {
       try {
@@ -89,11 +67,9 @@ public class UnlockedShortly extends DoorState {
           door.setState(new Locked(door));
           System.out.println("The door " + door.getId() + " is now locked (after short unlock).");
         }
-        // If the door is open after the timer, it remains in the UnlockedShortly state,
-        // which might be an unintended behavior. It will not auto-lock.
+
       } catch (InterruptedException e) {
-        // This block is executed if the timer thread is interrupted.
-        // The business logic implies that an interruption suggests the door is propped.
+
         door.setState(new Propped(door));
       }
     }).start();
